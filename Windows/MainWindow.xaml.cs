2c559a385ebe2c29f3d4379ce06229d790d9210f
T4M-Moling <clovermoling@gmail.com>
2021-08-26 21:21:08 +0800

    Add PanuonUI.Silver Compress background image

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -12,13 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Panuon.UI.Silver;
 
 namespace White_Obsidian_Minecraft_Launcher
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : WindowX
     {
         public MainWindow()
         {
