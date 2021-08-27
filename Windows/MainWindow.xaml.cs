557a0f803829126ac44f29bf1430c0f29dd63eeb
T4M-Moling <clovermoling@gmail.com>
2021-08-27 20:45:23 +0800

    Recovery to .NET 4.7 to build window

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Panuon.UI.Silver;
 
 namespace White_Obsidian_Minecraft_Launcher
@@ -26,6 +14,14 @@ namespace White_Obsidian_Minecraft_Launcher
             InitializeComponent();
         }
 
+        public void InitializationFunctionGrid()//初始化功能网格
+        {
+            MenuGrid.Visibility = Visibility.Hidden;
+            SettingGrid.Visibility = Visibility.Hidden;
+            AccountGrid.Visibility = Visibility.Hidden;
+            VersionGrid.Visibility = Visibility.Hidden;
+        }
+
         private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
         {
             // 设置工具提示可见性
@@ -34,22 +30,54 @@ namespace White_Obsidian_Minecraft_Launcher
             {
                 tt_home.Visibility = Visibility.Collapsed;
                 tt_setting.Visibility = Visibility.Collapsed;
+                tt_user.Visibility = Visibility.Collapsed;
+                tt_version.Visibility = Visibility.Collapsed;
             }
             else
             {
                 tt_home.Visibility = Visibility.Visible;
                 tt_setting.Visibility = Visibility.Visible;
+                tt_user.Visibility = Visibility.Visible;
+                tt_version.Visibility = Visibility.Visible;
             }
         }
 
         private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
         {
-            img_bg.Opacity = 1;
+            if (MenuGrid.Visibility == Visibility.Visible)
+            {
+                img_bg.Opacity = 1;
+                MenuGrid.Visibility = Visibility.Hidden;
+            }
+            if (SettingGrid.Visibility == Visibility.Visible)
+            {
+                if (AccountGrid.Visibility == Visibility.Visible)
+                {
+                    if (VersionGrid.Visibility == Visibility.Visible)
+                    {
+
+                    }
+                    else 
+                    {
+                        img_bg.Opacity = 1;
+                    }
+                }
+            }
         }
 
         private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
         {
-            img_bg.Opacity = 0.3;
+            if (SettingGrid.Visibility == Visibility.Hidden)
+            {
+                if (AccountGrid.Visibility == Visibility.Hidden)
+                {
+                    if (VersionGrid.Visibility == Visibility.Hidden)
+                    {
+                        img_bg.Opacity = 0.3;
+                        MenuGrid.Visibility = Visibility.Visible;
+                    }
+                }
+            }
         }
 
         private void BG_PreviewMouseLeftButtomDown(object sender, MouseButtonEventArgs e)
@@ -66,13 +94,50 @@ namespace White_Obsidian_Minecraft_Launcher
         {
             if(e.LeftButton == MouseButtonState.Pressed)
             {
-                this.DragMove();
+                DragMove();
             }
         }
 
         private void LaunchBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        public void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializationFunctionGrid();
+        }
+
+        private void MenuButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            InitializationFunctionGrid();
+            MenuGrid.Visibility = Visibility.Visible;
+            Tg_Btn.IsChecked = false;
+            img_bg.Opacity = 1;
+        }
+
+        private void SettingButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            InitializationFunctionGrid();
+            SettingGrid.Visibility = Visibility.Visible;
+            Tg_Btn.IsChecked = false;
+            img_bg.Opacity = 0.3;
+        }
+
+        private void AccountButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            InitializationFunctionGrid();
+            AccountGrid.Visibility = Visibility.Visible;
+            Tg_Btn.IsChecked = false;
+            img_bg.Opacity = 0.3;
+        }
+
+        private void VersionButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            InitializationFunctionGrid();
+            VersionGrid.Visibility = Visibility.Visible;
+            Tg_Btn.IsChecked = false;
+            img_bg.Opacity = 0.3;
         }
     }
 }
